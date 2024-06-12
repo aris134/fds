f3c1813c28030d33da10d31bd06277c1ed7e9a10
Aristotle Martin <aris32@uan-0002.head.cm.americas.sgi.com>
2024-06-12 13:21:30 +0000

    add v1 of gpu offloaded loop test

--- Utilities/Misc/loop_test/make_intel_cpu.sh
+++ Utilities/Misc/loop_test/make_intel_cpu.sh
@@ -1,1 +1,1 @@
-ifx -O3 -fiopenmp main.f90 -o loop3d
+ifx -O3 -fiopenmp main_v1.f90 -o loop3d
--- Utilities/Misc/loop_test/make_intel_gpu.sh
+++ Utilities/Misc/loop_test/make_intel_gpu.sh
@@ -1,1 +1,1 @@
-ifx -O3 -fiopenmp -fopenmp-targets=spir64 main.f90 -o loop3d
+ifx -O3 -fiopenmp -fopenmp-targets=spir64 main_v1.f90 -o loop3d
